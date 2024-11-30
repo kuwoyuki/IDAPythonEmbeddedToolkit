a5bf855d68b676374da6271a4e2124c7295deb05
kuwoyuki <kuwoyuki@gmail.com>
2024-12-01 00:07:38 +0600

    chore: upd for IDA Pro 9.0 and Python 3

--- define_code_functions.py
+++ define_code_functions.py
@@ -29,94 +29,135 @@
 ##############################################################################################
 import re
 import idaapi
+import ida_kernwin
+import ida_ida
+import ida_bytes
+import ida_search
+import ida_funcs
+import idc
 
 ################### USER DEFINED VALUES ###################
 # Enter a regular expression for how this architecture usually begins and ends functions.
 # If the architecture does not dictate how to start or end a function use r".*" to allow
 # for any instruction
 
-processor_name = idaapi.get_inf_structure().procname
+processor_name = ida_ida.inf_get_procname()
 
+if processor_name == "8051":
+    # 8051 Architecture Prologue and Epilogue
+    smart_prolog = re.compile(r".*")
+    smart_epilog = re.compile(r"reti{0,1}")
 
-if processor_name == '8051':
-	# 8051 Architecture Prologue and Epilogue
-	smart_prolog = re.compile(r".*")
-	smart_epilog = re.compile(r"reti{0,1}")
+elif processor_name == "PIC18Cxx":
+    # PIC18 Architecture Prologue and Epilogue
+    smart_prolog = re.compile(r".*")
+    smart_epilog = re.compile(r"return  0")
 
-elif processor_name == 'PIC18Cxx':
-	# PIC18 Architecture Prologue and Epilogue
-	smart_prolog = re.compile(r".*")
-	smart_epilog = re.compile(r"return  0")
+elif processor_name == "m32r":
+    # Mitsubishi M32R Architecture Prologue and Epilogue
+    smart_prolog = re.compile(r"push +lr")
+    smart_epilog = re.compile(r"jmp +lr.*")
 
-elif processor_name == 'm32r':
-	# Mitsubishi M32R Architecutre Prologue and Epilogue
-	smart_prolog = re.compile(r"push +lr")
-	smart_epilog = re.compile(r"jmp +lr.*")
+elif processor_name == "TMS32028":
+    # Texas Instruments TMS320C28x
+    smart_prolog = re.compile(r".*")
+    smart_epilog = re.compile(r"lretr")
 
-elif processor_name == 'TMS32028':
-	# Texas Instruments TMS320C28x
-	smart_prolog = re.compile(r".*")
-	smart_epilog = re.compile(r"lretr")
+elif processor_name == "AVR":
+    # AVR
+    smart_prolog = re.compile(r"push +r")
+    smart_epilog = re.compile(r"reti{0,1}")
 
-elif processor_name == 'AVR':
-	# AVR
-	smart_prolog = re.compile(r"push +r")
-	smart_epilog = re.compile(r"reti{0,1}")
+elif processor_name == "RH850":
+    # RH850
+    smart_prolog = re.compile(r"prepare \{.*\}, \d")
+    smart_epilog = re.compile(r"dispose \d, \{*.\}, [lp]")
 
-elif processor_name == 'RH850':
-	# RH850
-	smart_prolog = re.compile(r"prepare \{.*\}, \d")
-	smart_epilog = re.compile(r"dispose \d, \{*.\}, [lp]")
-
-elif processor_name == 'ARM':
-	# ARM
-	smart_prolog = re.compile(r"push \{R4,LR\}")
-	smart_epilog = re.compile(r"pop \{R4,PC\}")
+elif processor_name == "ARM":
+    # ARM
+    smart_prolog = re.compile(r"push \{R4,LR\}")
+    smart_epilog = re.compile(r"pop \{R4,PC\}")
 
 else:
-	print "[define_code_functions.py] UNSUPPORTED PROCESSOR. Processor = %s is unsupported. Exiting." % processor_name
-	raise NotImplementedError('Unsupported Processor Type.')
+    print(
+        f"[define_code_functions.py] UNSUPPORTED PROCESSOR. Processor = {processor_name} is unsupported. Exiting."
+    )
+    raise NotImplementedError("Unsupported Processor Type.")
 
-print "[define_code_functions.py] Processor = %s -- Reg Expressions Selected. Proceeding." % processor_name
+print(
+    f"[define_code_functions.py] Processor = {processor_name} -- Reg Expressions Selected. Proceeding."
+)
 ############################################################
 
-start_addr = ida_kernwin.ask_addr(ida_ida.inf_get_min_ea(), "Please enter the starting address for the data to be defined.")
-end_addr = ida_kernwin.ask_addr(ida_ida.inf_get_max_ea(), "Please enter the ending address for the data to be defined.")
+def main():
+    start_addr = ida_kernwin.ask_addr(
+        ida_ida.inf_get_min_ea(),
+        "Please enter the starting address for the data to be defined.",
+    )
+    end_addr = ida_kernwin.ask_addr(
+        ida_ida.inf_get_max_ea(),
+        "Please enter the ending address for the data to be defined.",
+    )
 
-if ((start_addr is not None and end_addr is not None) and (start_addr != BADADDR and end_addr != BADADDR)):
-	do_make_unk = ida_kernwin.ask_yn(0, "Do you want to make all of the code block UNKNOWN first?")
-	if (do_make_unk == 1):
-		curr_addr = start_addr
-		while (curr_addr < end_addr):
-			ida_bytes.del_items(curr_addr,idc.DELIT_SIMPLE)
-			curr_addr += 1
-	if (do_make_unk != -1):
-		curr_addr = start_addr
-		print "[make_code_functions.py] Running script to define code and functions on 0x%x to 0x%x" % (start_addr, end_addr)
-		while (curr_addr < end_addr):
-			next_unexplored = ida_search.find_unknown(curr_addr, idaapi.SEARCH_DOWN)
-			#print "0x%x" % next_unexplored
-			idc.create_insn(next_unexplored)		# We don't care whether it succeeds or fails so not storing retval
-			curr_addr = next_unexplored
+    if (
+        start_addr is None
+        or end_addr is None
+        or start_addr == idc.BADADDR
+        or end_addr == idc.BADADDR
+    ):
+        print(
+            "[make_code_functions.py] Quitting. Entered address values are not valid."
+        )
+        return
 
-		# Finished attempting to make all unexplored bytes into code
-		# Now, attempt to create functions of all code not currently in a function
-		print "[make_code_functions.py] Completed attempting to define bytes as code. Now trying to define functions."
-		curr_addr = start_addr
-		while (curr_addr != BADADDR and curr_addr < end_addr):
-			if (ida_bytes.is_code(ida_bytes.get_full_flags(curr_addr)) and idc.get_func_attr(curr_addr, FUNCATTR_START) == BADADDR):
-					#print "Function Stuffs 0x%0x" % curr_addr
-					if(smart_prolog.match(idc.generate_disasm_line(curr_addr, 0)) or smart_epilog.match(idc.generate_disasm_line(idc.prev_head(curr_addr), 0))):
-						#generate_disasm has a force parameter but we are not using it for now
-						#print "Smart Prolog match"
-						if (ida_funcs.add_func(curr_addr) != 0):	
-							# MakeFunction(curr_addr) was successful so set curr_addr to next addr after the new function
-							curr_addr = idc.get_func_attr(curr_addr, FUNCATTR_END)	# Returns first address AFTER the end of the function
-							continue
-			curr_addr = idc.next_head(curr_addr)
-else:
-	print "[make_code_functions.py] Quitting. Entered address values are not valid."
-	
-		
+    do_make_unk = ida_kernwin.ask_yn(
+        0, "Do you want to make all of the code block UNKNOWN first?"
+    )
 
-        
+    if do_make_unk == -1:  # User cancelled
+        return
+
+    if do_make_unk == 1:
+        curr_addr = start_addr
+        while curr_addr < end_addr:
+            ida_bytes.del_items(curr_addr, idc.DELIT_SIMPLE)
+            curr_addr += 1
+
+    curr_addr = start_addr
+    print(
+        f"[make_code_functions.py] Running script to define code and functions on 0x{start_addr:x} to 0x{end_addr:x}"
+    )
+
+    # First pass: Convert unknown bytes to code
+    while curr_addr < end_addr:
+        next_unexplored = ida_search.find_unknown(curr_addr, idaapi.SEARCH_DOWN)
+        idc.create_insn(next_unexplored)
+        curr_addr = next_unexplored
+
+	# Finished attempting to make all unexplored bytes into code
+	# Now, attempt to create functions of all code not currently in a function
+    print(
+        "[make_code_functions.py] Completed attempting to define bytes as code. Now trying to define functions."
+    )
+    curr_addr = start_addr
+    while curr_addr != idc.BADADDR and curr_addr < end_addr:
+        if (
+            ida_bytes.is_code(ida_bytes.get_full_flags(curr_addr))
+            and idc.get_func_attr(curr_addr, idc.FUNCATTR_START) == idc.BADADDR
+        ):
+
+            if smart_prolog.match(
+                idc.generate_disasm_line(curr_addr, 0)
+            ) or smart_epilog.match(
+                idc.generate_disasm_line(idc.prev_head(curr_addr), 0)
+            ):
+
+                if ida_funcs.add_func(curr_addr):
+                    curr_addr = idc.get_func_attr(curr_addr, idc.FUNCATTR_END)
+                    continue
+
+        curr_addr = idc.next_head(curr_addr)
+
+
+if __name__ == "__main__":
+    main()
--- make_strings.py
+++ make_strings.py
@@ -1,17 +1,17 @@
 ##############################################################################################
 # Copyright 2017 The Johns Hopkins University Applied Physics Laboratory LLC
 # All rights reserved.
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this 
-# software and associated documentation files (the "Software"), to deal in the Software 
-# without restriction, including without limitation the rights to use, copy, modify, 
-# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 #
 # 2020-08-07 - modified to work on IDA 7.x - Alexander Pick (alx@pwn.su)
@@ -25,46 +25,102 @@
 # and attempts to define it as a string.
 #
 # Input: 	start_addr: 	Start address for range to search for strings
-#			end_addr:		End address for range to search for strings
+# 			end_addr:		End address for range to search for strings
 #
 ##############################################################################################
+import ida_kernwin
+import ida_ida
+import ida_bytes
+import ida_nalt
+import idc
 
 ################### USER DEFINED VALUES ###################
-min_length = 5           			# Minimum number of characters needed to define a string       
-string_end = [0x00]		# Possible "ending characters" for strings. A string will not be 
-                                    # defined if it does not end with one of these characters
+MIN_LENGTH = 5  # Minimum number of characters needed to define a string
+STRING_END = [0x00]  # Possible "ending characters" for strings. A string will not be
+# defined if it does not end with one of these characters
 ###########################################################
 
-start_addr = ida_kernwin.ask_addr(ida_ida.inf_get_min_ea(), "Please enter the starting address for the data to be analyzed.")
-end_addr = ida_kernwin.ask_addr(ida_ida.inf_get_max_ea(), "Please enter the ending address for the data to be analyzed.")
 
-if ((start_addr is not None and end_addr is not None) and (start_addr != BADADDR and end_addr != BADADDR) and start_addr < end_addr):
-	string_start = start_addr
-	print "[make_strings.py] STARTING. Attempting to make strings with a minimum length of %d on data in range 0x%x to 0x%x" % (min_length, start_addr, end_addr)
-	num_strings = 0
-	while string_start < end_addr:
-		num_chars = 0
-		curr_addr = string_start
-		while curr_addr < end_addr:
-			byte = idc.get_wide_byte(curr_addr)
-			if ((byte < 0x7F and byte > 0x1F) or byte in (0x9, 0xD, 0xA)):		# Determine if a byte is a "character" based on this ASCII range
-				num_chars += 1
-				curr_addr += 1			
-			else:
-				if ((byte in string_end) and (num_chars >= min_length)):
-					ida_bytes.del_items(string_start, curr_addr - string_start, DELIT_SIMPLE)
-					if (ida_bytes.create_strlit(string_start, 0, ida_nalt.STRTYPE_TERMCHR) == 1): #get_inf_attr(INF_STRTYPE)
-						print "[make_strings.py] String created at 0x%x to 0x%x" % (string_start, curr_addr)
-						num_strings += 1
-						string_start = curr_addr
-						break
-					else:
-						#print "[make_strings.py] String create FAILED at 0x%x to 0x%x" % (string_start, curr_addr)
-						break
-				else:		
-					# String does not end with one of the defined "ending characters", does not meet the minimum string length, or is not an ASCII character
-					break
-		string_start += 1
-	print "[make_strings.py] FINISHED. Created %d strings in range 0x%x to 0x%x" % (num_strings, start_addr, end_addr)
-else:
-	print "[make_strings.py] QUITTING. Entered address values not valid."
+def make_strings(start_addr, end_addr):
+    """
+    Search for and create strings in the specified address range.
+
+    Args:
+        start_addr: Start address for range to search
+        end_addr: End address for range to search
+    """
+    string_start = start_addr
+    num_strings = 0
+
+    print(
+        f"[make_strings.py] STARTING. Attempting to make strings with a minimum length of {MIN_LENGTH} "
+        f"on data in range 0x{start_addr:x} to 0x{end_addr:x}"
+    )
+
+    while string_start < end_addr:
+        num_chars = 0
+        curr_addr = string_start
+
+        while curr_addr < end_addr:
+            byte = idc.get_wide_byte(curr_addr)
+
+            # Check if byte is printable ASCII or common control character
+            if (0x20 <= byte < 0x7F) or byte in (0x9, 0xD, 0xA):
+                num_chars += 1
+                curr_addr += 1
+            else:
+                if byte in STRING_END and num_chars >= MIN_LENGTH:
+                    # Undefine the current range of bytes
+                    ida_bytes.del_items(
+                        string_start, curr_addr - string_start, ida_bytes.DELIT_SIMPLE
+                    )
+
+                    # Try to create a string
+                    if ida_bytes.create_strlit(
+                        string_start, 0, ida_nalt.STRTYPE_TERMCHR
+                    ):
+                        print(
+                            f"[make_strings.py] String created at 0x{string_start:x} to 0x{curr_addr:x}"
+                        )
+                        num_strings += 1
+                        string_start = curr_addr
+                        break
+                    else:
+                        break
+                else:
+                    # String does not end with one of the defined "ending characters", does not meet the minimum string length, or is not an ASCII character
+                    break
+
+        string_start += 1
+
+    print(
+        f"[make_strings.py] FINISHED. Created {num_strings} strings in range 0x{start_addr:x} to 0x{end_addr:x}"
+    )
+
+
+def main():
+    """Main function to get user input and run string detection."""
+    start_addr = ida_kernwin.ask_addr(
+        ida_ida.inf_get_min_ea(),
+        "Please enter the starting address for the data to be analyzed.",
+    )
+    end_addr = ida_kernwin.ask_addr(
+        ida_ida.inf_get_max_ea(),
+        "Please enter the ending address for the data to be analyzed.",
+    )
+
+    if (
+        start_addr is None
+        or end_addr is None
+        or start_addr == idc.BADADDR
+        or end_addr == idc.BADADDR
+        or start_addr >= end_addr
+    ):
+        print("[make_strings.py] QUITTING. Entered address values not valid.")
+        return
+
+    make_strings(start_addr, end_addr)
+
+
+if __name__ == "__main__":
+    main()
